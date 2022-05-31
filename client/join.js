7eb73d69364b058902b3299991bfe25697ddddc1
dahye-program <dahye0729@sch.ac.kr>
2022-05-31 19:27:27 +0900

    Add : join

---
+++ client/join.js
@@ -0,0 +1,29 @@
+const joinId = document.getElementById('id-value');
+const joinPw = document.getElementById('pw-value');
+const joinName = document.getElementById('name-value');
+const joinDate = document.getElementById('date-value');
+const goMainBtn = document.getElementById('go-client');
+const joinCheckBtn = document.getElementById('join-check');
+
+goMainBtn.addEventListener('click', ()=>{
+  location.href = 'client.html'
+})
+
+joinCheckBtn.addEventListener('click', async function(){
+  await fetch("http://localhost:5500/join",{
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      id : joinId.value,
+      pw: joinPw.value,
+      name: joinName.value,
+      date : joinDate.value
+    })
+  }).then(res => {
+    return res.json()
+  }).then(res => {
+    console.log(res);
+  })
+})
--- server/index.js
+++ server/index.js
@@ -14,9 +14,18 @@ app.use(cors({
 app.post('/login', (req, res)=>{
   // DB에 SELECT 쿼리문 날려서 받아온 값과 비교
   if(req.body.id === 'dahye' && req.body.pw === '1234'){
-    return res.json('ok');
+    return res.json('Login OK');
   }
-  else return res.json('false');
+  else return res.json('Login False');
+})
+
+app.post('/join', (req, res)=>{
+  // DB에 INSERT
+  // console.log(req.body.id);
+  // console.log(req.body.pw);
+  // console.log(req.body.name);
+  // console.log(req.body.date);
+  return res.json('Join OK');
 })
 
 const server = app.listen(5500, ()=>{
